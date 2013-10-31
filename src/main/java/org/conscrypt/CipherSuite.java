10c5b40023b197d305ddd2fbb6229b1acf6bf314
Alex Klyubin <klyubin@google.com>
2013-10-31 14:36:06 -0700

    Adjust the default list of ciphers suites of SSLEngine.
    SSLEngine should use the same cipher suites by default as SSLSocket
    whose list was recently cleaned up. One complication is that the
    current SSLEngine implementation does not support EC.
    
    This CL removes cipher suites with bulk encryption cipher key length
    shorter than 80 bits and adds 256-bit versions of AES cipher suites.
    
    Bug: 11220570
    Change-Id: I81fb34d8067a8565c0ae11883bb5c1ee65ed6875

--- src/main/java/org/conscrypt/CipherSuite.java
+++ src/main/java/org/conscrypt/CipherSuite.java
@@ -734,18 +734,14 @@ public class CipherSuite {
                 SSL_RSA_WITH_RC4_128_MD5,
                 SSL_RSA_WITH_RC4_128_SHA,
                 TLS_RSA_WITH_AES_128_CBC_SHA,
+                TLS_RSA_WITH_AES_256_CBC_SHA,
                 TLS_DHE_RSA_WITH_AES_128_CBC_SHA,
+                TLS_DHE_RSA_WITH_AES_256_CBC_SHA,
                 TLS_DHE_DSS_WITH_AES_128_CBC_SHA,
+                TLS_DHE_DSS_WITH_AES_256_CBC_SHA,
                 SSL_RSA_WITH_3DES_EDE_CBC_SHA,
                 SSL_DHE_RSA_WITH_3DES_EDE_CBC_SHA,
-                SSL_DHE_DSS_WITH_3DES_EDE_CBC_SHA,
-                SSL_RSA_WITH_DES_CBC_SHA,
-                SSL_DHE_RSA_WITH_DES_CBC_SHA,
-                SSL_DHE_DSS_WITH_DES_CBC_SHA,
-                SSL_RSA_EXPORT_WITH_RC4_40_MD5,
-                SSL_RSA_EXPORT_WITH_DES40_CBC_SHA,
-                SSL_DHE_RSA_EXPORT_WITH_DES40_CBC_SHA,
-                SSL_DHE_DSS_EXPORT_WITH_DES40_CBC_SHA
+                SSL_DHE_DSS_WITH_3DES_EDE_CBC_SHA
                 };
         count = 0;
         for (int i = 0; i < defaultCipherSuites.length; i++) {
