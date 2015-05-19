236d0fcc9cf9710fdf8dd000d2bc50eede25d784
Kenny Root <kroot@google.com>
2015-05-19 16:05:07 -0700

    EVP_AEAD: change constant to work with non-BoringSSL
    If the version of OpenSSL we're compiling against has the EVP_AEAD
    interface ported, then we can use it. So check for one of its
    definitions in addition to checking for BoringSSL. To be more precise,
    rename the constant that is generated to directly reflect what it's
    being used for.
    
    Change-Id: Ic47eecff4a3968c56b32dc30867fc29cd89fdb6a

--- src/gen/native/generate_constants.cc
+++ src/gen/native/generate_constants.cc
@@ -21,6 +21,7 @@
 #include <openssl/rsa.h>
 #include <openssl/ssl.h>
 #include <openssl/x509v3.h>
+#include <openssl/evp.h>
 
 static const char kCopyright[] =
     "/* Copyright (C) 2015 The Android Open Source Project\n"
@@ -44,8 +45,8 @@ int main(int /* argc */, char ** /* argv */) {
   printf("package org.conscrypt;\n\n");
   printf("public final class NativeConstants {\n");
 
-  printf("    public static final boolean IS_BORINGSSL = %s;\n",
-#if defined(OPENSSL_IS_BORINGSSL)
+  printf("    public static final boolean HAS_EVP_AEAD = %s;\n",
+#if defined(EVP_AEAD_DEFAULT_TAG_LENGTH) || defined(OPENSSL_IS_BORINGSSL)
          "true"
 #else
          "false"
