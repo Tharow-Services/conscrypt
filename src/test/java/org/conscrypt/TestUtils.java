2b5b5be6f77faf7a95b5b48cea228d8bc25f2bd7
Paul Lietar <lietar@google.com>
2015-07-21 09:27:41 +0100

    Add method to delete extension from a certificate
    The OpenSSLX509Certificate is still immutable. Instead a modified copy is returned.
    The use case for this is recreating the TBS component of a Precertificate as
    described by RFC6962 section 3.2.
    
    Change-Id: I2a9305ae7464642910decaf5ab46121a6f15d722

---
+++ src/test/java/org/conscrypt/TestUtils.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2015 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.conscrypt;
+
+import junit.framework.Assert;
+import java.util.Arrays;
+
+public class TestUtils extends Assert {
+    protected TestUtils() {
+    }
+
+    public static void assertEqualByteArrays(byte[] expected, byte[] actual) {
+        assertEquals(Arrays.toString(expected), Arrays.toString(actual));
+    }
+
+    public static void assertEqualByteArrays(byte[][] expected, byte[][] actual) {
+        assertEquals(Arrays.deepToString(expected), Arrays.deepToString(actual));
+    }
+
+    public static void assertContains(String actualValue, String expectedSubstring) {
+        if (actualValue == null) {
+            return;
+        }
+        if (actualValue.contains(expectedSubstring)) {
+            return;
+        }
+        fail("\"" + actualValue + "\" does not contain \"" + expectedSubstring + "\"");
+    }
+}
