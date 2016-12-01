dd9f6785a5d8c27acd8b8e47f87f192b36f9cb03
Kenny Root <kroot@google.com>
2016-12-01 13:04:09 -0800

    Create abstract base for OpenSSL-backed SSLSession
    This will aid in creating an SSL*-backed implementation of the Java
    language SSLSession class for use when we have not established a session
    already during handshake or when in False Start.
    
    Test: cts-tradefed run cts -m CtsLibcoreOkHttpTestCases -a arm64-v8a
    Test: cts-tradefed run cts -m CtsLibcoreTestCases -a arm64-v8a
    Change-Id: I58cfbee2940412beaed4a8d4d38211ecfbce7ec9

--- platform/src/main/java/org/conscrypt/TrustManagerImpl.java
+++ platform/src/main/java/org/conscrypt/TrustManagerImpl.java
@@ -401,8 +401,8 @@ public final class TrustManagerImpl extends X509ExtendedTrustManager {
 
     private byte[] getOcspDataFromSession(SSLSession session) {
         List<byte[]> ocspResponses = null;
-        if (session instanceof OpenSSLSessionImpl) {
-            OpenSSLSessionImpl opensslSession = (OpenSSLSessionImpl) session;
+        if (session instanceof OpenSSLAbstractSession) {
+            OpenSSLAbstractSession opensslSession = (OpenSSLAbstractSession) session;
             ocspResponses = opensslSession.getStatusResponses();
         } else {
             Method m_getResponses;
@@ -428,8 +428,8 @@ public final class TrustManagerImpl extends X509ExtendedTrustManager {
     }
 
     private byte[] getTlsSctDataFromSession(SSLSession session) {
-        if (session instanceof OpenSSLSessionImpl) {
-            OpenSSLSessionImpl opensslSession = (OpenSSLSessionImpl) session;
+        if (session instanceof OpenSSLAbstractSession) {
+            OpenSSLAbstractSession opensslSession = (OpenSSLAbstractSession) session;
             return opensslSession.getTlsSctData();
         }
 
