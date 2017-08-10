daa9b76017a7d3068ec796e43147e2af5ed521c2
Robert Sloan <varomodt@google.com>
2017-08-09 17:21:12 -0700

    Don't run ASAN on conscrypt variable reification.
    ... because it doesn't make any sense to asan our code generation.
    
    Test: make -j100 checkbuild ASAN_OPTIONS=detect_leaks=0 SANITIZE_HOST=address FORCE_BUILD_LLVM_COMPONENTS=true
    Change-Id: I2dc86488513d91a59deb502eea623a1b2c09aee6

--- Android.mk
+++ Android.mk
@@ -63,6 +63,7 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := constants/src/gen/cpp/generate_constants.cpp
 LOCAL_MODULE := conscrypt_generate_constants
 LOCAL_SHARED_LIBRARIES := libcrypto libssl
+LOCAL_SANITIZE := never
 include $(BUILD_HOST_EXECUTABLE)
 
 conscrypt_generate_constants_exe := $(LOCAL_INSTALLED_MODULE)
