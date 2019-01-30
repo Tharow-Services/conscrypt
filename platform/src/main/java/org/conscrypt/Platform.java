f1efba4c47c53aa799fa1923d6b4ad84d468a209
Pete Bentley <prb@google.com>
2019-01-30 16:35:51 +0000

    Make Platform.getDefaultProviderName public on Android.
    Now that Conscrypt is on the boot class path, the reflexive call[1]
    from a CTS test to a package private method fails due to
    differing class loaders.  This change solves it by making the
    method public (but not visible to the SDK), or we could also
    do it by just using setAccessible before calling if that
    seems preferable.
    
    [1] https://android.googlesource.com/platform/external/conscrypt/+/master/
    repackaged/testing/src/main/java/com/android/org/conscrypt/TestUtils.java#190
    
    Bug: 123182286
    Test: atest CtsLibcoreTestCases:com.android.org.conscrypt.ct.CTVerifierTest
    Test: atest CtsLibcoreTestCases:com.android.org.conscrypt.CertPinManagerTest
    Change-Id: I2f3fac6b09d689cd1312af3fbc2504ec35a5a959

--- platform/src/main/java/org/conscrypt/Platform.java
+++ platform/src/main/java/org/conscrypt/Platform.java
@@ -84,7 +84,7 @@ final class Platform {
      * Default name used in the {@link java.security.Security JCE system} by {@code OpenSSLProvider}
      * if the default constructor is used.
      */
-    static String getDefaultProviderName() {
+    public static String getDefaultProviderName() {
         return "AndroidOpenSSL";
     }
 
