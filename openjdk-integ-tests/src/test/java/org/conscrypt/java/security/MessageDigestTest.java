429740bc6c3351e5a7b951923ac6ba0ec07bac17
Daulet Zhanguzin <dauletz@google.com>
2020-02-05 10:44:07 +0000

    Cherry-pick 6 commits to android10-tests-dev
    These commits add additional tests for the Conscrypt module.
    
    94b7d630 Add ServiceTester helper class (#661)
    057f7558 Generalize Signature tests (#696)
    d555e759 Add tests for X509CRL implementation (#698)
    6bdfb523 Improve testing of KeyFactory classes (#699)
    6b4bf398 Simplify some tests so they pass on Android (#702)
    f89ae777 Skip BC keys in EC KeyFactory tests (#704)
    
    Bug: 139310440
    Test: cts -m CtsLibcoreTestCases
    Test: cts -m CtsLibcoreOkHttpTestCases
    Change-Id: Iabbeb8bc8f66b20ab10eb606cecc178c025baae6

--- openjdk-integ-tests/src/test/java/org/conscrypt/java/security/MessageDigestTest.java
+++ openjdk-integ-tests/src/test/java/org/conscrypt/java/security/MessageDigestTest.java
@@ -21,7 +21,6 @@ import static org.junit.Assert.assertEquals;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.Provider;
-import java.security.Security;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
@@ -33,6 +32,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import dalvik.system.VMRuntime;
 import sun.security.jca.Providers;
+import tests.util.ServiceTester;
 
 @RunWith(JUnit4.class)
 public final class MessageDigestTest {
@@ -69,16 +69,10 @@ public final class MessageDigestTest {
 
     @Test
     public void test_getInstance() throws Exception {
-        Provider[] providers = Security.getProviders();
-        for (Provider provider : providers) {
-            Set<Provider.Service> services = provider.getServices();
-            for (Provider.Service service : services) {
-                String type = service.getType();
-                if (!type.equals("MessageDigest")) {
-                    continue;
-                }
-                String algorithm = service.getAlgorithm();
-                try {
+        ServiceTester.test("MessageDigest")
+            .run(new ServiceTester.Test() {
+                @Override
+                public void test(Provider provider, String algorithm) throws Exception {
                     // MessageDigest.getInstance(String)
                     MessageDigest md1 = MessageDigest.getInstance(algorithm);
                     assertEquals(algorithm, md1.getAlgorithm());
@@ -95,12 +89,8 @@ public final class MessageDigestTest {
                     assertEquals(algorithm, md3.getAlgorithm());
                     assertEquals(provider, md3.getProvider());
                     test_MessageDigest(md3);
-                } catch (Exception e) {
-                    throw new Exception("Problem testing MessageDigest." + algorithm
-                            + " from provider " + provider.getName(), e);
                 }
-            }
-        }
+            });
     }
 
     private static final Map<String, Map<String, byte[]>> EXPECTATIONS
