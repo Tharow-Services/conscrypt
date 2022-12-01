16bc62428af7b032e8f0633a7fc326cde65d9687
Miguel <miguelaranda@google.com>
2022-12-01 14:27:22 +0000

    Add conscrypt updatable certificates.
    This cl adds the new blueprint files required for certificate loading, and an additional ca_certificates_apex build rule used to create the prebuild modules for loading certificates. While we are currently have to out all certificates within Conscrypt's apex build rules, we intend to later avoid that step.
    
    Test: atest TrustedCertificateStoreTest
    (cherry picked from https://android-review.googlesource.com/q/commit:f90f112e43a942ec59b707fcf9606b0030a04b75)
    Merged-In: I49eade243362a76900ec54ed2642bac9f61ab15c
    Change-Id: I49eade243362a76900ec54ed2642bac9f61ab15c

--- repackaged/platform/src/main/java/com/android/org/conscrypt/TrustedCertificateStore.java
+++ repackaged/platform/src/main/java/com/android/org/conscrypt/TrustedCertificateStore.java
@@ -85,8 +85,12 @@ import javax.security.auth.x500.X500Principal;
 @libcore.api.CorePlatformApi
 @Internal
 public class TrustedCertificateStore implements ConscryptCertStore {
+<<<<<<< HEAD   (5864a5 Update test certs for CertBlocklistTest)
 
     private static final String PREFIX_SYSTEM = "system:";
+=======
+    private static String PREFIX_SYSTEM = "system:";
+>>>>>>> CHANGE (f90f11 Add conscrypt updatable certificates.)
     private static final String PREFIX_USER = "user:";
 
     public static final boolean isSystem(String alias) {
@@ -105,7 +109,12 @@ public class TrustedCertificateStore implements ConscryptCertStore {
         static {
             String ANDROID_ROOT = System.getenv("ANDROID_ROOT");
             String ANDROID_DATA = System.getenv("ANDROID_DATA");
-            defaultCaCertsSystemDir = new File(ANDROID_ROOT + "/etc/security/cacerts");
+            File updatableDir = new File("/apex/com.android.conscrypt/cacerts");
+            if (updatableDir.exists()) {
+                defaultCaCertsSystemDir = updatableDir;
+            } else {
+                defaultCaCertsSystemDir = new File(ANDROID_ROOT + "/etc/security/cacerts");
+            }
             setDefaultUserDirectory(new File(ANDROID_DATA + "/misc/keychain"));
         }
     }
@@ -135,6 +144,10 @@ public class TrustedCertificateStore implements ConscryptCertStore {
             public TrustedCertificateStore() {
         this(PreloadHolder.defaultCaCertsSystemDir, PreloadHolder.defaultCaCertsAddedDir,
                 PreloadHolder.defaultCaCertsDeletedDir);
+    }
+
+    public TrustedCertificateStore(File baseDir) {
+        this(baseDir, PreloadHolder.defaultCaCertsAddedDir, PreloadHolder.defaultCaCertsDeletedDir);
     }
 
     public TrustedCertificateStore(File systemDir, File addedDir, File deletedDir) {
