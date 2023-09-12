cddbecbf21d73b53369a83685284fa2111c6e53c
Pete Bentley <prb@google.com>
2023-09-12 17:43:31 +0100

    Update TrustedCertificateStore Apex logic.
    1. Split the logic into multiple sub-methods that can be unit tested.
    2. Add a flag file mechanism for disabling Apex certs
    3. Add an end-to-end test script.
    
    Bug: 264540110
    Test: TrustedCertificateStoreTest
    Test: platform/src/script/install_test_cert.sh
    Change-Id: I36588ac3b0c7888ef5f8c0649a828e0802d52109

---
+++ platform/src/script/ListTestCerts.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+
+import java.security.KeyStore;
+import java.security.cert.X509Certificate;
+import java.util.Enumeration;
+
+public class ListTestCerts {
+
+  public static void main(String[] args) throws Exception {
+    KeyStore ks = KeyStore.getInstance("AndroidCAStore");
+    ks.load(null);
+
+    Enumeration<String> aliases = ks.aliases();
+
+    int count = 0;
+    while (aliases.hasMoreElements()) {
+      String next = aliases.nextElement();
+      X509Certificate cert = (X509Certificate) ks.getCertificate(next);
+      String name = cert.getSubjectX500Principal().getName();
+      if (name.contains("Conscrypt")) {
+        System.out.println(name);
+        count++;
+      }
+    }
+    System.out.printf("Found %d matching certs\n", count);
+  }
+}
