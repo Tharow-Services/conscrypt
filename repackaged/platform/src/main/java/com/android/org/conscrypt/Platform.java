ebd0c2235e7c5a8c4c745aafb6b755459adae77d
Miguel Aranda <miguelaranda@google.com>
2023-10-19 12:32:48 +0000

    Revert "Revert "Remove TLS 1.0 and 1.1 from the list of default protocols.""
    This reverts commit 9d107a651d2f5a2f21be080388fe327a494c8c23.
    
    Reason for revert: https://android-review.git.corp.google.com/c/platform/libcore/+/2796415 has solved the underlying issues
    (cherry picked from https://android-review.googlesource.com/q/commit:7b36b7db3850b2b506df796de2895d203e87755b)
    Merged-In: I389024a1af44b63c32c3c742dfacaf143d421f98
    Change-Id: I389024a1af44b63c32c3c742dfacaf143d421f98

--- repackaged/platform/src/main/java/com/android/org/conscrypt/Platform.java
+++ repackaged/platform/src/main/java/com/android/org/conscrypt/Platform.java
@@ -562,4 +562,8 @@ final class Platform {
     public static boolean isJavaxCertificateSupported() {
         return true;
     }
+
+    public static boolean isTlsV1Deprecated() {
+        return false;
+    }
 }
