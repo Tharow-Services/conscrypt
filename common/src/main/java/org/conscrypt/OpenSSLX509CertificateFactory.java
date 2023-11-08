7f81d8018ed1fc90b6a9595ca8f5fd951f621271
Miguel Aranda <miguelaranda@google.com>
2023-11-08 17:17:40 +0000

    Changes to cert parsing.
    Throws ParsingError when no certificates are found in an input stream, and allows comments to be added within pem cert files.
    
    Change-Id: Ie989b4a6cabad2a12dbbfd4cfb5326a8b5fee1e1

--- common/src/main/java/org/conscrypt/OpenSSLX509CertificateFactory.java
+++ common/src/main/java/org/conscrypt/OpenSSLX509CertificateFactory.java
@@ -41,6 +41,9 @@ public class OpenSSLX509CertificateFactory extends CertificateFactorySpi {
     private static final byte[] PKCS7_MARKER = new byte[] {
             '-', '-', '-', '-', '-', 'B', 'E', 'G', 'I', 'N', ' ', 'P', 'K', 'C', 'S', '7'
     };
+    private static final byte[] PEM_MARKER = new byte[] {
+            '-', '-', '-', '-', '-', 'B', 'E', 'G', 'I', 'N', ' ', 'C', 'E', 'R', 'T', 'I'
+    };
 
     private static final int PUSHBACK_SIZE = 64;
 
@@ -112,9 +115,9 @@ public class OpenSSLX509CertificateFactory extends CertificateFactorySpi {
 
             final PushbackInputStream pbis = new PushbackInputStream(inStream, PUSHBACK_SIZE);
             try {
-                final byte[] buffer = new byte[PKCS7_MARKER.length];
+                byte[] buffer = new byte[PKCS7_MARKER.length];
 
-                final int len = pbis.read(buffer);
+                int len = pbis.read(buffer);
                 if (len < 0) {
                     /* No need to reset here. The stream was empty or EOF. */
                     throw new ParsingException("inStream is empty");
@@ -131,9 +134,25 @@ public class OpenSSLX509CertificateFactory extends CertificateFactorySpi {
                         return null;
                     }
                     return certs.get(0);
-                } else {
-                    return fromX509DerInputStream(pbis);
                 }
+                T x509DerCert = fromX509DerInputStream(pbis);
+                if (x509DerCert != null) {
+                  return x509DerCert;
+                }
+                while (len == PKCS7_MARKER.length) {
+                    throw new ParsingException("AAAAAAAA");
+                //   len = pbis.read(buffer);
+                //   for (int i = 0; i < len; ++i) {
+                //     if (buffer[i] == '-') {
+                //       pbis.unread(buffer, i, len - i);
+                //       T pemCert = fromX509PemInputStream(pbis);
+                //       if (pemCert != null) {
+                //         return pemCert;
+                //       }
+                //     }
+                //   }
+                }
+                return null;
             } catch (Exception e) {
                 if (markable) {
                     try {
@@ -223,6 +242,8 @@ public class OpenSSLX509CertificateFactory extends CertificateFactorySpi {
                 }
             } while (c != null);
 
+            if (coll.size() == 0)
+              throw new ParsingException("No certificates found");
             return coll;
         }
 
