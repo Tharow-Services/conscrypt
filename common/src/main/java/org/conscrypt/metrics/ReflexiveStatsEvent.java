3eb8769b519411383034395c653711b7da720093
Miguel Aranda <miguelaranda@google.com>
2024-01-31 13:20:54 +0000

    Change TLS metric logging to single uid
    Change-Id: Ia6f255d43461632e16a84e23000d70bb5559ad60

--- common/src/main/java/org/conscrypt/metrics/ReflexiveStatsEvent.java
+++ common/src/main/java/org/conscrypt/metrics/ReflexiveStatsEvent.java
@@ -24,14 +24,10 @@ import org.conscrypt.Internal;
 public class ReflexiveStatsEvent {
     private static final OptionalMethod newBuilder;
     private static final Class<?> c_statsEvent;
-    private static final Object sdkVersion;
-    private static final boolean sdkVersionBiggerThan32;
 
     static {
-        sdkVersion = getSdkVersion();
         c_statsEvent = initStatsEventClass();
         newBuilder = new OptionalMethod(c_statsEvent, "newBuilder");
-        sdkVersionBiggerThan32 = (sdkVersion != null) && ((int) sdkVersion > 32);
     }
 
     private static Class<?> initStatsEventClass() {
@@ -57,7 +53,7 @@ public class ReflexiveStatsEvent {
     }
 
     public static ReflexiveStatsEvent buildEvent(int atomId, boolean success, int protocol,
-            int cipherSuite, int duration, int source, int[] uids) {
+            int cipherSuite, int duration, int source, int uid) {
         ReflexiveStatsEvent.Builder builder = ReflexiveStatsEvent.newBuilder();
         builder.setAtomId(atomId);
         builder.writeBoolean(success);
@@ -65,9 +61,7 @@ public class ReflexiveStatsEvent {
         builder.writeInt(cipherSuite);
         builder.writeInt(duration);
         builder.writeInt(source);
-        if (sdkVersionBiggerThan32) {
-          builder.writeIntArray(uids);
-        }
+        builder.writeInt(uid);
         builder.usePooledBuffer();
         return builder.build();
     }
@@ -104,7 +98,6 @@ public class ReflexiveStatsEvent {
         private static final OptionalMethod writeInt;
         private static final OptionalMethod build;
         private static final OptionalMethod usePooledBuffer;
-        private static final OptionalMethod writeIntArray;
 
         static {
             c_statsEvent_Builder = initStatsEventBuilderClass();
@@ -113,7 +106,6 @@ public class ReflexiveStatsEvent {
             writeInt = new OptionalMethod(c_statsEvent_Builder, "writeInt", int.class);
             build = new OptionalMethod(c_statsEvent_Builder, "build");
             usePooledBuffer = new OptionalMethod(c_statsEvent_Builder, "usePooledBuffer");
-            writeIntArray = new OptionalMethod(c_statsEvent_Builder, "writeIntArray", int[].class);
         }
 
         private static Class<?> initStatsEventBuilderClass() {
@@ -147,11 +139,6 @@ public class ReflexiveStatsEvent {
 
         public void usePooledBuffer() {
             usePooledBuffer.invoke(this.builder);
-        }
-
-        public Builder writeIntArray(final int[] values) {
-            writeIntArray.invoke(this.builder, values);
-            return this;
         }
 
         public ReflexiveStatsEvent build() {
