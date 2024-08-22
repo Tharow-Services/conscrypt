544f41c6aeea735dd8628b3329970b084aa2f468
Miguel Aranda <miguelaranda@google.com>
2024-08-22 13:05:46 +0000

    [WIP] Conscrypt javagen metrics
    Change-Id: I98658262e68f3621de5b875fa292cb529cd0eeec

---
+++ repackaged/common/src/main/java/com/android/org/conscrypt/metrics33/ConscryptStatsLog.java
@@ -0,0 +1,31 @@
+/* GENERATED SOURCE. DO NOT MODIFY. */
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.org.conscrypt.metrics;
+
+import com.android.org.conscrypt.metrics.GeneratedStatsLog;
+
+public final class ConscryptStatsLog {
+    public static final int TLS_HANDSHAKE_REPORTED = 317;
+
+    private ConscryptStatsLog() {}
+
+    public static void write(int atomId, boolean success, int protocol, int cipherSuite,
+            int duration, Source source, int[] uids) {
+        GeneratedStatsLog.write(atomId, success, protocol, cipherSuite,
+            duration, source.ordinal(), uids);
+    }
+}
