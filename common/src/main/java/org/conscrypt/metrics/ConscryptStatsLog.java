d4bb6dd41f56a4466125538e038f42fcba35ca11
Miguel Aranda <miguelaranda@google.com>
2024-10-11 11:06:02 +0000

    Add statslog java library to conscrypt
    This is needed to log metrics to statsd.
    
    Change-Id: I524f07a864c88272fc5b4044644679836abca82b

--- common/src/main/java/org/conscrypt/metrics/ConscryptStatsLog.java
+++ common/src/main/java/org/conscrypt/metrics/ConscryptStatsLog.java
@@ -15,6 +15,7 @@
  */
 package org.conscrypt.metrics;
 
+import org.conscrypt.metrics.GeneratedStatsLog;
 import org.conscrypt.Internal;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.Executors;
@@ -37,6 +38,7 @@ import java.lang.Thread.UncaughtExceptionHandler;
  *   --module conscrypt \
  *   --javaPackage org.conscrypt.metrics \
  *   --javaClass ConscryptStatsLog
+ * @hide This class is not part of the Android public SDK API
  **/
 @Internal
 public final class ConscryptStatsLog {
@@ -61,17 +63,42 @@ public final class ConscryptStatsLog {
 
     private ConscryptStatsLog() {}
 
+    private static final Object sdkVersion;
+    private static final boolean sdkVersionBiggerThan32;
+
+    static {
+        sdkVersion = getSdkVersion();
+        sdkVersionBiggerThan32 = (sdkVersion != null) && ((int) sdkVersion > 32);
+    }
+
+    @SuppressWarnings("NewApi")
     public static void write(int atomId, boolean success, int protocol, int cipherSuite,
             int duration, Source source, int[] uids) {
-        e.execute(new Runnable() {
+      e.execute(new Runnable() {
             @Override
             public void run() {
-                ReflexiveStatsEvent event = ReflexiveStatsEvent.buildEvent(
-                        atomId, success, protocol, cipherSuite, duration,
-                        source.ordinal(), uids);
-
-                ReflexiveStatsLog.write(event);
+                if (sdkVersionBiggerThan32) {
+                    GeneratedStatsLog.write(atomId, success, protocol, cipherSuite,
+                        duration, source.ordinal(), uids);
+                } else {
+                    ReflexiveStatsEvent event = ReflexiveStatsEvent.buildEvent(
+                            atomId, success, protocol, cipherSuite, duration,
+                            source.ordinal(), uids);
+                    ReflexiveStatsLog.write(event);
+                }
             }
         });
+    }
+
+
+    static Object getSdkVersion() {
+        try {
+            OptionalMethod getSdkVersion =
+                    new OptionalMethod(Class.forName("dalvik.system.VMRuntime"),
+                                        "getSdkVersion");
+            return getSdkVersion.invokeStatic();
+        } catch (ClassNotFoundException e) {
+            return null;
+        }
     }
 }
