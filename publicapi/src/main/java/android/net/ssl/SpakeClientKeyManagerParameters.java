b0bed28e413776cc5ae1fa9d580534ab1578720a
Sorin Basca <sorinbasca@google.com>
2024-12-03 14:29:09 +0000

    DO NOT SUBMIT Add SPAKE2+ API - copy for @FlaggedApi
    Bug: 382233100
    Test: mmm external/conscrypt
    Test: atest CtsLibcoreTestCases:android.net.ssl.SpakeIdentityTest
    Test: atest CtsLibcoreTestCases:android.net.ssl.SpakeClientKeyManagerParametersTest
    Test: atest CtsLibcoreTestCases:android.net.ssl.SpakeServerKeyManagerParametersTest
    Change-Id: Iefb8c7a05785e35d0e518ff94c11384e96e2acd5

---
+++ publicapi/src/main/java/android/net/ssl/SpakeClientKeyManagerParameters.java
@@ -0,0 +1,150 @@
+/*
+ * Copyright (C) 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.net.ssl;
+
+import static java.util.Objects.requireNonNull;
+
+import android.annotation.FlaggedApi;
+
+import libcore.util.NonNull;
+
+import javax.net.ssl.ManagerFactoryParameters;
+
+/**
+ * This class is used to provide specific data to a SPAKE2+ {@link
+ * javax.net.ssl.KeyManagerFactory} for a client.
+ */
+@FlaggedApi(com.android.org.conscrypt.flags.Flags.FLAG_SPAKE2PLUS_API)
+public final class SpakeClientKeyManagerParameters implements ManagerFactoryParameters {
+    private SpakeIdentity spakeIdentity;
+    private byte[] clientPassword;
+    private byte[] pakeContext;
+
+    private SpakeClientKeyManagerParameters() {}
+
+    /**
+     * Builder for {@link SpakeClientKeyManagerParameters}.
+     *
+     * Before running {@link build}, the identity, password and context must be set through {@link
+     * setIdentity}, {@link setClientPassword} and {@link setPakeContext} respectively.
+     */
+    public static final class Builder {
+        private SpakeIdentity spakeIdentity;
+        private byte[] clientPassword;
+        private byte[] pakeContext;
+
+        public Builder() {}
+
+        /**
+         * Sets the client's identity using individual components.
+         *
+         * @param clientIdentity The client's identity.
+         * @param serverIdentity The server's identity.
+         * @return This builder.
+         */
+        @NonNull
+        public Builder setIdentity(@NonNull byte[] clientIdentity, @NonNull byte[] serverIdentity) {
+            return setIdentity(new SpakeIdentity(clientIdentity, serverIdentity));
+        }
+
+        /**
+         * Sets the identity using a {@link SpakeIdentity} object.
+         *
+         * @param spakeIdentity The combined identity of the client and server.
+         * @return This builder.
+         */
+        @NonNull
+        public Builder setIdentity(@NonNull SpakeIdentity spakeIdentity) {
+            requireNonNull(spakeIdentity, "The identity needs to be set");
+            this.spakeIdentity = spakeIdentity;
+            return this;
+        }
+
+        /**
+         * Sets the shared password.
+         *
+         * @param password The shared password.
+         * @return This builder.
+         */
+        @NonNull
+        public Builder setClientPassword(@NonNull byte[] password) {
+            requireNonNull(password, "The password needs to be set");
+            clientPassword = password;
+            return this;
+        }
+
+        /**
+         * Sets the PAKE context.
+         *
+         * @param context The PAKE context.
+         * @return This builder.
+         */
+        @NonNull
+        public Builder setPakeContext(@NonNull byte[] context) {
+            requireNonNull(context, "The context needs to be set");
+            pakeContext = context;
+            return this;
+        }
+
+        /**
+         * Builds the {@link SpakeClientKeyManagerParameters}.
+         *
+         * @return The built parameters.
+         */
+        @NonNull
+        public SpakeClientKeyManagerParameters build() {
+            requireNonNull(spakeIdentity, "The identity needs to be set");
+            requireNonNull(clientPassword, "The password needs to be set");
+            requireNonNull(pakeContext, "The context needs to be set");
+
+            SpakeClientKeyManagerParameters params = new SpakeClientKeyManagerParameters();
+
+            params.spakeIdentity = spakeIdentity;
+            params.clientPassword = clientPassword;
+            params.pakeContext = pakeContext;
+
+            return params;
+        }
+    }
+
+    /**
+     * Gets the combined identity of the client and server.
+     *
+     * @return The {@link SpakeIdentity}.
+     */
+    public @NonNull SpakeIdentity getSpakeIdentity() {
+        return spakeIdentity;
+    }
+
+    /**
+     * Gets the client's password.
+     *
+     * @return The client's password.
+     */
+    public @NonNull byte[] getClientPassword() {
+        return clientPassword;
+    }
+
+    /**
+     * Gets the PAKE context.
+     *
+     * @return The PAKE context.
+     */
+    public @NonNull byte[] getPakeContext() {
+        return pakeContext;
+    }
+}
---
+++ publicapi/src/main/java/android/net/ssl/SpakeServerKeyManagerParameters.java
@@ -0,0 +1,149 @@
+/*
+ * Copyright (C) 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.net.ssl;
+
+import static java.util.Objects.requireNonNull;
+
+import android.annotation.FlaggedApi;
+
+import com.android.org.conscrypt.ByteArray;
+
+import libcore.util.NonNull;
+import libcore.util.Nullable;
+
+import java.security.InvalidParameterException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.net.ssl.ManagerFactoryParameters;
+
+/**
+ * This class is used to provide specific data to a SPAKE2+ {@link
+ * javax.net.ssl.KeyManagerFactory} for a server.
+ */
+@FlaggedApi(com.android.org.conscrypt.flags.Flags.FLAG_SPAKE2PLUS_API)
+public final class SpakeServerKeyManagerParameters implements ManagerFactoryParameters {
+    private Map<ByteArray, byte[]> passwords;
+    private byte[] pakeContext;
+
+    private SpakeServerKeyManagerParameters() {}
+
+    /**
+     * Builder for {@link SpakeServerKeyManagerParameters}.
+     *
+     * Before running {@link build}, the context must be set through {@link setPakeContex} and there
+     * mustbe at least one mapping set through {@link addPasswordMapping}.
+     */
+    public final static class Builder {
+        private Map<ByteArray, byte[]> passwords = new HashMap<>();
+        private byte[] pakeContext;
+
+        public Builder() {}
+
+        /**
+         * Adds a password mapping for a client and server identity pair.
+         *
+         * @param clientIdentity The client's identity.
+         * @param serverIdentity The server's identity.
+         * @param password The password to use for the client and server identities.
+         * @return This builder.
+         */
+        @NonNull
+        public Builder addPasswordMapping(@NonNull byte[] clientIdentity,
+                @NonNull byte[] serverIdentity, @NonNull byte[] password) {
+            return addPasswordMapping(new SpakeIdentity(clientIdentity, serverIdentity), password);
+        }
+
+        /**
+         * Adds a password mapping for a {@link SpakeIdentity}.
+         *
+         * @param spakeIdentity The combined identity of the client and server.
+         * @param password The password to use for the given identity.
+         * @return This builder.
+         */
+        @NonNull
+        public Builder addPasswordMapping(
+                @NonNull SpakeIdentity spakeIdentity, @NonNull byte[] password) {
+            requireNonNull(spakeIdentity, "The identity needs to be set");
+            requireNonNull(password, "The password needs to be set");
+            passwords.put(spakeIdentity.combineIdentities(), password);
+            return this;
+        }
+
+        /**
+         * Sets the PAKE context to use for the server.
+         *
+         * @param pakeContext The PAKE context to use.
+         * @return This builder.
+         */
+        @NonNull
+        public Builder setPakeContext(@NonNull byte[] pakeContext) {
+            this.pakeContext = pakeContext;
+            return this;
+        }
+
+        /**
+         * Builds the {@link SpakeServerKeyManagerParameters}.
+         *
+         * @return The built parameters.
+         * @throws InvalidParameterException if no password mappings have been added.
+         */
+        @NonNull
+        public SpakeServerKeyManagerParameters build() {
+            if (passwords.isEmpty()) {
+                throw new InvalidParameterException();
+            }
+            requireNonNull(pakeContext, "The context needs to be set");
+
+            SpakeServerKeyManagerParameters params = new SpakeServerKeyManagerParameters();
+            params.passwords = passwords;
+            params.pakeContext = pakeContext;
+            return params;
+        }
+    }
+
+    /**
+     * Gets the password for a client and server identity pair.
+     *
+     * @param clientIdentity The client's identity.
+     * @param serverIdentity The server's identity.
+     * @return The password, or null if no password is found.
+     */
+    public @Nullable byte[] getPassword(
+            @NonNull byte[] clientIdentity, @NonNull byte[] serverIdentity) {
+        return getPassword(new SpakeIdentity(clientIdentity, serverIdentity));
+    }
+
+    /**
+     * Gets the password for a {@link SpakeIdentity}.
+     *
+     * @param spakeIdentity The combined identity of the client and server.
+     * @return The password, or null if no password is found.
+     */
+    public @Nullable byte[] getPassword(@NonNull SpakeIdentity spakeIdentity) {
+        return passwords.get(spakeIdentity.combineIdentities());
+    }
+
+    /**
+     * Gets the PAKE context to use for the server.
+     *
+     * @return The PAKE context, or null if no context has been set.
+     */
+    public @NonNull byte[] getPakeContext() {
+        return pakeContext;
+    }
+}
