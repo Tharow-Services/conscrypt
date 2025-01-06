abd5c778813df943f5bdad9cb04dc3ee7a086468
Miguel Aranda <miguelaranda@google.com>
2025-01-06 01:21:42 -0800

    Add Spake to Conscrypt
    Change-Id: I07be873c127b31a6f1272b00cb4198a8a547d9c7

---
+++ repackaged/common/src/main/java/com/android/org/conscrypt/Spake2PlusTrustManager.java
@@ -0,0 +1,33 @@
+/* GENERATED SOURCE. DO NOT MODIFY. */
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package com.android.org.conscrypt;
+
+import javax.net.ssl.TrustManager;
+
+/**
+ * @hide This class is not part of the Android public SDK API
+ */
+@Internal
+public class Spake2PlusTrustManager implements TrustManager {
+
+  Spake2PlusTrustManager() {}
+
+  public void checkClientTrusted() {}
+
+  public void checkServerTrusted() {}
+}
