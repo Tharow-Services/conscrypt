248d8ce06cf8fb9bdd4e58e330b88abcedd6fccd
Miguel Aranda <miguelaranda@google.com>
2025-01-14 02:21:51 -0800

    Add Spake parameter plumbing to Conscrypt
    This cl:
    - Adds Key/TrustManagerFactories and Key/TrustManagers for Spake
    - Modifies OpenSSLParameters to accept Spake
    - Adds Spake setup within NativeSSL
    - Includes miscellaneous changes to make the above work
    - Adds tests for the above.
    
    TEST=atest SSLSocketTest/SpakeManagerFactoriesTest/SpakeKeyManagerParametersTest
    BUG=382233100
    
    Change-Id: I5203a651fa74fe90cabd234bc2b1a94cfc9d3e88

--- repackaged/common/src/test/java/com/android/org/conscrypt/DuckTypedHpkeSpiTest.java
+++ repackaged/common/src/test/java/com/android/org/conscrypt/DuckTypedHpkeSpiTest.java
@@ -249,7 +249,8 @@ public class DuckTypedHpkeSpiTest {
         assertTrue(duckTyped.getDelegate() instanceof HpkeForeignSpi);
 
         // And that it is delegating to a real HpkeImpl, so we can test it.
-        assertNotNull(duckTyped.getDelegate());
+        HpkeForeignSpi foreign = (HpkeForeignSpi) duckTyped.getDelegate();
+        assertTrue(foreign.realSpi instanceof HpkeImpl);
     }
 
     // Provides HpkeContext instances that use a "foreign" SPI, that is one that isn't
